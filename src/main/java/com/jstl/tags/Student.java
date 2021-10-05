8c765aa36f28f5cde1b60b3ef82adb24669d182a
m2ddd <72919230+m2ddd@users.noreply.github.com>
2021-10-05 18:35:00 +0530

    Update Student.java

--- src/main/java/com/jstl/tags/Student.java
+++ src/main/java/com/jstl/tags/Student.java
@@ -4,12 +4,14 @@ public class Student {
 	private String fname;
 	private String lname;
 	private boolean goldCustomer;
+	private customerid;
 	// constructor
 	public Student(String fname, String lname, boolean goldCustomer) {
 		super();
 		this.fname = fname;
 		this.lname = lname;
 		this.goldCustomer = goldCustomer;
+		
 	}
 	// getters and setters
 	public String getFname() {	return fname;	}
@@ -18,6 +20,8 @@ public class Student {
 	public void setLname(String lname) {	this.lname = lname;	}
 	public boolean isGoldCustomer() {	return goldCustomer;	}
 	public void setGoldCustomer(boolean goldCustomer) {	this.goldCustomer = goldCustomer;}
+	public String getcustomerid() {	return customerid;	}
+	public String setcustomerid() {	this.customerid = customerid;	}
 	
 	
 }
